afaab307ab81c579bdde02c1019d2d1ddf3a4be7
DanielPayet <danieldu974@gmail.com>
2017-03-15 19:54:26 +0100

    mercredi 20h

--- core/src/com/daniel/game/PacmanGame.java
+++ core/src/com/daniel/game/PacmanGame.java
@@ -1,19 +1,32 @@
 package com.daniel.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.math.Vector2;
+import com.daniel.game.model.Maze;
+import com.daniel.game.model.Pacman;
+import com.daniel.game.model.World;
 import com.daniel.game.screens.GameScreen;
+import com.daniel.game.view.WorldRenderer;
 
 public class PacmanGame extends ApplicationAdapter {
 	
 	GameScreen gs;
+	World world;
+	WorldRenderer renderer;
+	Maze maze;
+	Pacman pacman;
 	
 	public void create(){
-		gs = new GameScreen();
-		gs.create();
+		world = new World();
+		renderer = new WorldRenderer(world);
+		maze = new Maze(world);
+		pacman = new Pacman(new Vector2((30-23)*16,14*16), world);
+		gs = new GameScreen(world,renderer);
+		world.getMaze().loadDemoLevel();
 	}
 	
 	public void render(){
-		gs.render();
+		renderer.render();
 	}
 	
 	public void dispose(){
--- core/src/com/daniel/game/screens/GameScreen.java
+++ core/src/com/daniel/game/screens/GameScreen.java
@@ -1,10 +1,7 @@
 package com.daniel.game.screens;
 
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.math.Vector2;
 import com.daniel.game.model.GameElement;
-import com.daniel.game.model.Maze;
-import com.daniel.game.model.Pacman;
 import com.daniel.game.model.World;
 import com.daniel.game.view.WorldRenderer;
 
@@ -12,16 +9,15 @@ public class GameScreen  implements Screen {
 	private World world;
 	private WorldRenderer renderer;
 	
-	public GameScreen(){
-		
+	public GameScreen(World world, WorldRenderer renderer){
+		this.world = world;
+		this.renderer = renderer;
 	}
 	
 	public void show(){
-		
 	}
 	
 	public void render(float x){
-		renderer.render();
 	}
 	
 	public void resize(int a ,int b){
@@ -44,17 +40,5 @@ public class GameScreen  implements Screen {
 		for(GameElement ge : world){
 			ge.getTexture().dispose();
 		}
-	}
-
-	public void create() {
-		world = new World();
-		renderer = new WorldRenderer(world);
-		world.setMaze(new Maze(world));
-		world.setPacman(new Pacman(new Vector2((30-23)*16,14*16), world));
-		world.getMaze().loadDemoLevel();
-	}
-
-	public void render() {
-		renderer.render();
 	}
 }
